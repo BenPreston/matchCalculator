2aa9efd336d6f0784f5d8597a47ccf0db3da9757
Ben Preston <benpreston@pop-os.localdomain>
2020-09-18 02:59:01 +0200

    calculating data only done one second level and it definitely needs to be refactored as not DRY

--- main.js
+++ main.js
@@ -4,19 +4,16 @@ console.log(data);
 const homeTeam = "Chelsea LFC";
 const awayTeam = "Manchester City WFC";
 
-// Data stored here
+// DATA STORAGE
 
-// 1. When event happened and who had it to detemine flow of the game
-const homeTeamMins = {};
-const awayTeamMins = {};
+// Logs the detail of what each player has done
+// Has a limitation in my view in that for example unsuccessful passes don't seem to be recorded
+const playerEventTally = {};
 
-const calcTeamMins = (team, homeOrAway) => {};
+const shots = [];
 
-// Patern Data to determine where play comes from
-const homeTeamPlayPatt = {};
-const awayTeamPlayPatt = {};
-
-const calcPlayPattern = (team, homeOrAway, eventType, playPattern) => {
+// Can be used to calculate a specific match event. May not be useful as evolved the modle
+const calcMatchEvent = (team, homeOrAway, eventType, playPattern) => {
   if (team === homeOrAway) {
     eventType[playPattern] >= 0
       ? eventType[playPattern]++
@@ -24,28 +21,87 @@ const calcPlayPattern = (team, homeOrAway, eventType, playPattern) => {
   }
 };
 
+// Creates events with players like the player event tally
+const calcEventWithPlayer = (event) => {
+  if (event.type.name === "Shot") shots.push(event);
+
+  if (event.player && event.player.name && event.type && event.type.name) {
+    const player = event["player"]["name"];
+    const type = event["type"]["name"];
+
+    if (!playerEventTally[player]) playerEventTally[player] = {};
+    if (!playerEventTally[player]["team"])
+      playerEventTally[player]["team"] = event["team"]["name"];
+    if (!playerEventTally["position"])
+      playerEventTally[player]["position"] = event["position"]["name"];
+
+    playerEventTally[player][type] >= 0
+      ? (playerEventTally[player][type] += 1)
+      : (playerEventTally[player][type] = 1);
+  }
+};
+
+// Creates pass (and ideally other detail) based on each player
+const calcPlayerPassDetail = (event) => {
+  if (event.player && event.player.name && event.pass) {
+    const player = event["player"]["name"];
+    const pass = event["pass"];
+
+    if (!playerEventTally[player]["pass_detail"])
+      playerEventTally[player]["pass_detail"] = {
+        body_part: {},
+        height: {},
+        recipient: {},
+        total_length: 0,
+      };
+
+    if (pass["body_part"] && pass["body_part"]["name"]) {
+      playerEventTally[player]["pass_detail"]["body_part"][pass.body_part.name]
+        ? (playerEventTally[player]["pass_detail"]["body_part"][
+            pass.body_part.name
+          ] += 1)
+        : (playerEventTally[player]["pass_detail"]["body_part"][
+            pass.body_part.name
+          ] = 1);
+    }
+
+    if (pass["height"] && pass["height"]["name"]) {
+      playerEventTally[player]["pass_detail"]["height"][pass.height.name]
+        ? (playerEventTally[player]["pass_detail"]["height"][
+            pass.height.name
+          ] += 1)
+        : (playerEventTally[player]["pass_detail"]["height"][
+            pass.height.name
+          ] = 1);
+    }
+
+    if (pass["recipient"] && pass["recipient"]["name"]) {
+      playerEventTally[player]["pass_detail"]["recipient"][pass.recipient.name]
+        ? (playerEventTally[player]["pass_detail"]["recipient"][
+            pass.recipient.name
+          ] += 1)
+        : (playerEventTally[player]["pass_detail"]["recipient"][
+            pass.recipient.name
+          ] = 1);
+    }
+
+    if (pass["length"]) {
+      playerEventTally[player]["pass_detail"]["total_length"] += pass["length"];
+    }
+  }
+};
+
 const makeMatchData = () => {
   data.map((event) => {
-    const team = event["possession_team"]["name"];
+    const type = event["type"]["name"];
 
-    const minutes = event["minute"];
-    const playPattern = event["play_pattern"]["name"];
-
-    // Minutes calculator
-    calcPlayPattern(team, homeTeam, homeTeamMins, minutes);
-    calcPlayPattern(team, awayTeam, awayTeamMins, minutes);
-
-    calcPlayPattern(team, homeTeam, homeTeamPlayPatt, playPattern);
-    calcPlayPattern(team, awayTeam, awayTeamPlayPatt, playPattern);
+    calcEventWithPlayer(event);
+    calcPlayerPassDetail(event);
   });
 };
 
 makeMatchData();
 
-console.log(homeTeamMins);
-console.log(awayTeamMins);
+console.log(playerEventTally);
 
-// console.log(homeTeamPlayPatt);
-// console.log(awayTeamPlayPatt);
-
-//
+console.log(shots[0].shot);
